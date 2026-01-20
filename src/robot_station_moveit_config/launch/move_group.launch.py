66fc0fd68d4e5934aa47383fc290167d3f17f005
YiminHu26 <yimin.hu@student.kit.edu>
2026-01-20 19:45:03 +0100

    Added cell_control and moveit_config packages

---
+++ src/robot_station_moveit_config/launch/move_group.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_move_group_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("station_ur_orbbec_robotiq2f85", package_name="robot_station_moveit_config").to_moveit_configs()
+    return generate_move_group_launch(moveit_config)
